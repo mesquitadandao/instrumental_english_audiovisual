5faf07834927fd5cae7c6fc4018b3bb07f3e960d
mesquitadandao <mesquitadandao@users.noreply.github.com>
2014-01-06 22:34:51 -0300

    #2 add bing translator

--- javascripts/instrumental-english-audiovisual.js
+++ javascripts/instrumental-english-audiovisual.js
@@ -11,3 +11,50 @@
 	split(/\s/).
 	uniq();
 };
+
+var Translation = function() {
+	this.HOST = 'http://api.microsofttranslator.com/v2/ajax.svc/TranslateArray2';
+	this.APPID = 'THxUiA69g9O-X27g2Er6pTPh_G4-yOmfeNGwVI4ISTDc*';
+	this.WORDS = [];
+	this.TEXTS = null;
+	this.FROM = 'en';
+	this.TRANSLATING = false;
+
+	this.words = function(words){
+		this.TEXTS = '["'+words.join('","')+'"]';
+		this.WORDS = words;
+		return this;
+	};
+
+	this.from = function(languageCode){
+		this.FROM = languageCode;
+		return this;
+	};
+
+	this.to = function(languageCode){
+		var script = 'HOST?appId=APPID&oncomplete=TRANSLATION._continue&onerror=TRANSLATION._break&texts=TEXTS&from=FROM&to=TO'.
+		gsub(/HOST/, this.HOST).gsub(/APPID/, this.APPID).gsub(/TEXTS/, this.TEXTS).
+		gsub(/FROM/, this.FROM).gsub(/TO/, languageCode);
+		this.TRANSLATING = true;
+		$.getScript(script);
+	};
+
+	this._continue = function(response){
+		var translations = [];
+		for(var i = 0; i < response.length; i++){
+			translations.push({"origin":this.WORDS[i], "translation":response[i].TranslatedText.toLowerCase()});
+		}
+		this.success(translations);
+		this.TRANSLATING = false;
+	};
+
+	this._break = function(response){
+		this.fail();
+		this.TRANSLATING = false;
+	};
+
+	this.translating = function(){
+		return this.TRANSLATING;
+	};
+}
+var TRANSLATION = new Translation();
--- test/asset/js/qunit/instrumental-english-audiovisual.js
+++ test/asset/js/qunit/instrumental-english-audiovisual.js
@@ -1,8 +1,21 @@
 module('extractUniqueWordsEnglish');
 
-test('return Array [i,will,i\'m,you,go,want,now,english,is,not].', function() {
+test('return Array ["i","will","i\'m","you","go","want","now","english","is","not"].', function() {
 	var expected = ["i","will","i\'m","you","go","want","now","english","is","not"]+"";
 
 	var extracted = "I will. I'm. You go. I want. Go now. 'English' is not \"english\", 2014/01/01 :-) ;_".extractUniqueWordsEnglish()+"";
 	equal(extracted, expected, 'when send in String "I will. I\'m. You go. I want. Go now. \'English\' is not "english", 2014/01/01 :-) ;_"');
+});
+
+module('Translation');
+
+test('return Array [{"origin":"i", "translation":"eu"},{"origin":"you", "translation":"você"}].', function() {
+	var expected = [{"origin":"i", "translation":"eu"},{"origin":"you", "translation":"você"}]+"";
+	var translateds = [];
+	Translation.prototype.success = function(translations){
+		translateds = translations;
+	}
+	TRANSLATION.words(["i","you"]).to("pt");
+	while(TRANSLATION.translating()){};
+	equal(translateds, expected, 'when send TRANSLATION.words(["i","you"]).to("pt")');
 });

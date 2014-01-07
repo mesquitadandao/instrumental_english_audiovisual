421a6cfa4326090e932fb5a6184406fbbd1dc554
mesquitadandao <mesquitadandao@users.noreply.github.com>
2014-01-06 23:14:25 -0300

    #2 created vocabulary

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,8 +2,18 @@ $(function(){
 	var $fullText = $("#full-text");
 	var $extractUniqueWords = $("#extract-unique-words");
 	var $uniqueWords = $("#unique-words");
-	
+	var $vocabulary = $("#vocabulary");
+	var $vocabularyBody = $vocabulary.find('tbody');
+
+	Translation.prototype.success = function(translations){
+		for(var i = 0; i < translations.length; i++){
+			$vocabularyBody.append('<tr><td>'+translations[i].origin+'</td><td>'+translations[i].translation+'</td><tr>');
+		};
+	}
+
 	$extractUniqueWords.on("click.extract", function(){
-		$uniqueWords.val($fullText.val().extractUniqueWordsEnglish());		
+		$vocabularyBody.empty();
+		var extrateds = $fullText.val().extractUniqueWordsEnglish();
+		TRANSLATION.words(extrateds).to("pt");
 	});
 });

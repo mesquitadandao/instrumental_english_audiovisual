4d96efe03aa5dd07bfeb1529af8ef732326c2653
mesquitadandao <mesquitadandao@users.noreply.github.com>
2014-01-08 15:21:58 -0300

    #4 play all in sequence [origin => translation, origin => translation, ...].

--- javascripts/main.js
+++ javascripts/main.js
@@ -12,11 +12,11 @@ $(function(){
 			$this.attr('disabled','disabled').removeClass('fa-play').addClass('fa-pause');
 			var $parent = $this.parent();
 			var audio = lastTranslations[$parent.data("index")][$parent.data("key")].audio();
-			var next = lastTranslations[$parent.data("index")][$parent.data("key")].next
+			var next = lastTranslations[$parent.data("index")][$parent.data("key")].next || function(){};
 			if (next instanceof Next){
 				audio.onended = function(){$this.attr('disabled',null).removeClass('fa-pause').addClass('fa-play');next.exec();};
 			}else{
-				audio.onended = function(){$this.attr('disabled',null).removeClass('fa-pause').addClass('fa-play');};
+				audio.onended = function(){$this.attr('disabled',null).removeClass('fa-pause').addClass('fa-play'); next();};
 			}
 			audio.play();
 		});
@@ -30,8 +30,9 @@ $(function(){
 	};
 
 	var applyNextKey = function(key){
-		$(".play-"+key+"-all").on("click.play", function(){
+		$(".play-"+key+"-all").off("click.play").on("click.play", function(){
 			var $plays = $(".play-"+key);
+			var $this = $(this);
 			for(var i = 0; i < lastTranslations.length; i++){
 				if (i < lastTranslations.length - 1){
 					var next = $plays[i+1];
@@ -41,11 +42,47 @@ $(function(){
 						for(var j = 0; j < lastTranslations.length; j++){
 							lastTranslations[j][key].next = null;
 						}
+					$this.attr('disabled',null).removeClass('fa-pause').addClass('fa-play');
 					};
 				}
 			}
 			if($plays[0]){
+				$this.attr('disabled','disabled').removeClass('fa-play').addClass('fa-pause');
 				$plays[0].click();
+			}
+		});
+	};
+
+	var applyNextAll = function(){
+		$(".play-all").off("click.play").on("click.play", function(){
+			var $originPlays = $(".play-origin");
+			var $translationPlays = $(".play-translation");
+
+			var $this = $(this);
+			var $originTranslation = $('.play-origin-all, .play-translation-all');
+			for(var i = 0; i < lastTranslations.length; i++){
+				if (i < lastTranslations.length - 1){
+					var nextTranslation = $translationPlays[i];
+					lastTranslations[i].origin.next = new Next(nextTranslation);
+					var nextOrigin = $originPlays[i+1];
+					lastTranslations[i].translation.next = new Next(nextOrigin);
+				}else{
+					var nextTranslation = $translationPlays[i];
+					lastTranslations[i].origin.next = new Next(nextTranslation);
+					lastTranslations[i].translation.next = function(){
+						for(var j = 0; j < lastTranslations.length; j++){
+							lastTranslations[j].origin.next = null;
+							lastTranslations[j].translation.next = null;
+						}
+					$this.attr('disabled',null).removeClass('fa-pause').addClass('fa-play');
+					$originTranslation.attr('disabled',null).removeClass('fa-pause').addClass('fa-play');
+					};
+				}
+			}
+			if($originPlays[0]){
+				$this.attr('disabled','disabled').removeClass('fa-play').addClass('fa-pause');
+				$originTranslation.attr('disabled','disabled').removeClass('fa-play').addClass('fa-pause');
+				$originPlays[0].click();
 			}
 		});
 	};
@@ -58,6 +95,7 @@ $(function(){
 		applyAudio();
 		applyNextKey('origin');
 		applyNextKey('translation');
+		applyNextAll();
 	}
 
 	$extractUniqueWords.on("click.extract", function(){

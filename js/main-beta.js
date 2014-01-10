368b4efc89864b4843a4c75d358e2408308c8a6e
mesquitadandao <mesquitadandao@users.noreply.github.com>
2014-01-09 23:12:18 -0300

    #5 design.

--- js/main-beta.js
+++ js/main-beta.js
@@ -1,20 +1,23 @@
 $(function(){
 	var $yourText = $("#your-text");
-	var $yourVocabulary = $("#your-vocabulary");
-	var $yourVocabularyBody = $yourVocabulary.find('tbody');
+	var $yourVocabularyDiv = $("#your-vocabulary-div");
+	var $yourVocabularyBody = $("#your-vocabulary-body");
 	var lastTranslations = [];
 
 	var applyAudio = function(){
 		$(".play-origin, .play-translation").off("click.play").on("click.play", function(){
 			var $this = $(this);
-			$this.attr('disabled','disabled').removeClass('fa-play').addClass('fa-pause');
-			var $parent = $this.parent();
+			var $parent = $this.parent().parent();
+			$parent.addClass('success');
+			$this.attr('disabled','disabled').removeClass('glyphicon-play').addClass('glyphicon-volume-up');
 			var audio = lastTranslations[$parent.data("index")][$parent.data("key")].audio();
 			var next = lastTranslations[$parent.data("index")][$parent.data("key")].next || function(){};
 			if (next instanceof Next){
-				audio.onended = function(){$this.attr('disabled',null).removeClass('fa-pause').addClass('fa-play');next.exec();};
+				audio.onended = function(){$this.attr('disabled',null).removeClass('glyphicon-volume-up').addClass('glyphicon-play');$parent.removeClass('success');
+;next.exec();};
 			}else{
-				audio.onended = function(){$this.attr('disabled',null).removeClass('fa-pause').addClass('fa-play'); next();};
+				audio.onended = function(){$this.attr('disabled',null).removeClass('glyphicon-volume-up').addClass('glyphicon-play');$parent.removeClass('success');
+next();};
 			}
 			audio.play();
 		});
@@ -40,12 +43,12 @@ $(function(){
 						for(var j = 0; j < lastTranslations.length; j++){
 							lastTranslations[j][key].next = null;
 						}
-					$this.attr('disabled',null).removeClass('fa-pause').addClass('fa-play');
+					$this.attr('disabled',null).removeClass('glyphicon-volume-up').addClass('glyphicon-play');
 					};
 				}
 			}
 			if($plays[0]){
-				$this.attr('disabled','disabled').removeClass('fa-play').addClass('fa-pause');
+				$this.attr('disabled','disabled').removeClass('glyphicon-play').addClass('glyphicon-volume-up');
 				$plays[0].click();
 			}
 		});
@@ -72,14 +75,14 @@ $(function(){
 							lastTranslations[j].origin.next = null;
 							lastTranslations[j].translation.next = null;
 						}
-					$this.attr('disabled',null).removeClass('fa-pause').addClass('fa-play');
-					$originTranslation.attr('disabled',null).removeClass('fa-pause').addClass('fa-play');
+					$this.attr('disabled',null).removeClass('glyphicon-volume-up').addClass('glyphicon-play');
+					$originTranslation.attr('disabled',null).removeClass('glyphicon-volume-up').addClass('glyphicon-play');
 					};
 				}
 			}
 			if($originPlays[0]){
-				$this.attr('disabled','disabled').removeClass('fa-play').addClass('fa-pause');
-				$originTranslation.attr('disabled','disabled').removeClass('fa-play').addClass('fa-pause');
+				$this.attr('disabled','disabled').removeClass('glyphicon-play').addClass('glyphicon-volume-up');
+				$originTranslation.attr('disabled','disabled').removeClass('glyphicon-play').addClass('glyphicon-volume-up');
 				$originPlays[0].click();
 			}
 		});
@@ -89,14 +92,14 @@ $(function(){
 		if(translations.length > 0){
 			lastTranslations = translations;
 			for(var i = 0; i < lastTranslations.length; i++){
-				$yourVocabularyBody.append('<tr><td data-index="'+i+'" data-key="origin"><button class="fa fa-play play-origin"></button> '+lastTranslations[i].origin.text+'</td><td  data-index="'+i+'" data-key="translation"><button class="fa fa-play play-translation"></button> '+lastTranslations[i].translation.text+'</td><tr>');
+				$yourVocabularyBody.append('<tr><td data-index="'+i+'" data-key="origin"><h4><button class="btn btn-default glyphicon glyphicon-play play-origin"></button> '+lastTranslations[i].origin.text+'</h4></td><td  data-index="'+i+'" data-key="translation"><h4><button class="btn btn-default glyphicon glyphicon-play play-translation"></button> '+lastTranslations[i].translation.text+'</h4></td><tr>');
 			};
 			applyAudio();
 			applyNextKey('origin');
 			applyNextKey('translation');
 			applyNextAll();
 			$yourText.val("");
-			$yourVocabulary.show();
+			$yourVocabularyDiv.removeClass('hidden');
 		}else{
 			alert('Error');
 		}
